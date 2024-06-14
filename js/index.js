f1c86966cdf1f6ff5ae1c91c513c05423b7da99c
yoshfoydalanuvchi <yoshfoydalanuvchi@gmail.com>
2024-06-14 21:46:16 +0500

    none

---
+++ js/index.js
@@ -0,0 +1,73 @@
+//1
+// function ranglarYaratadigonMassiv(color1, color2, color3) {
+//   let res = [];
+//   res.push(color1);
+//   res.push(color2);
+//   res.push(color3);
+//   return res;
+// }
+
+// console.log(ranglarYaratadigonMassiv("qizil", "sariq", "yashil"));
+
+//2
+// function sonlarYaratadigonMassiv(number1, number2, number3) {
+//   let res = [];
+//   res.push(number1);
+//   res.push(number2);
+//   res.push(number3);
+//   return res;
+// }
+// console.log(sonlarYaratadigonMassiv(3, 4, 5));
+
+//Massiv metodlariga oid masalalar.
+
+//1
+// function oxirigaElementQoshish(arg, element) {
+//   arg.push(element);
+//   return arg;
+// }
+// console.log(oxirigaElementQoshish([1, 2, 3], 4));
+
+//2
+// function oxiridanElementOlish(arg, element) {
+//   arg.pop(element);
+//   return arg;
+// }
+// console.log(oxiridanElementOlish([1, 2, 3]));
+
+//3
+// function boshidanElementOlish(arg, element) {
+//   arg.shift(element);
+//   return arg;
+// }
+// console.log(boshidanElementOlish([1, 2, 3]));
+
+//4
+// function boshigaElementQoshish(arg, element) {
+//   arg.unshift(element);
+//   return arg;
+// }
+// console.log(boshigaElementQoshish([1, 2, 3], 0));
+
+//5
+// function elementTekshirish(array, element) {
+//   return array.includes(element);
+// }
+
+// console.log(elementTekshirish([1, 2, 3, 4], 3));
+
+//6
+// function birinchiIndex(matn, substring) {
+//   return matn.indexOf(substring);
+// }
+// console.log(birinchiIndex(["sher", "tumsoh", "quyon", "tuya"], "quyon"));
+
+//7
+// function oxirgiIndex(matn, substring) {
+//   return matn.lastIndexOf(substring);
+// }
+// console.log(
+//   oxirgiIndex(["sher", "tumsoh", "quyon", "tuya", "quyon", "sher"], "quyon")
+// );
+
+//8
